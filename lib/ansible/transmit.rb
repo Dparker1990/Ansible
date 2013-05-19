22601d6b11eb5c0e4fc639b2776d104f0287ec66
Derek Parker <parkerderek86@gmail.com>
2013-05-18 21:04:56 -0500

    Use Proc for define_method body

--- lib/ansible/transmit.rb
+++ lib/ansible/transmit.rb
@@ -14,17 +14,21 @@ module Ansible
       @name = name
       _beacons << beacon_name.to_sym
 
-      define_method beacon_name do
-        response.headers['Content-Type'] = 'text/event-stream'
-
-        render nothing: true
-      end
+      define_method beacon_name, beacon_action
     end
 
     private
 
     def beacon_name
       "#{@name}_ansible_beacon"
+    end
+
+    def beacon_action
+      Proc.new do
+        response.headers['Content-Type'] = 'text/event-stream'
+
+        render nothing: true
+      end
     end
   end
 end
