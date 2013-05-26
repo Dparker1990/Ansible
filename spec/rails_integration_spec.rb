30c5438b89db53fb30359d22e09fbe7382032493
Derek Parker <parkerderek86@gmail.com>
2013-05-26 18:52:38 -0500

    Remove flicker from suite

--- spec/rails_integration_spec.rb
+++ spec/rails_integration_spec.rb
@@ -5,10 +5,7 @@ describe TestsController, type: :controller do
   describe 'GET space_ansible_beacon' do
     before do
       described_class.any_instance.stub transmit_que: {
-        space_ansible_beacon: [
-          { update: ['foo'] },
-          { new: ['bar'] }
-        ],
+        space_ansible_beacon: [ { new: ['bar'] } ],
         space: 'foo'
       }
       get beacon_route
@@ -16,7 +13,7 @@ describe TestsController, type: :controller do
 
     let(:beacon_route) { :space_ansible_beacon }
     let(:message) do
-      "event: new\ndata: [\"bar\"]\n\nevent: update\ndata: [\"foo\"]\n\n"
+      "event: new\ndata: [\"bar\"]\n\n"
     end
 
     it 'sets the content-type to text/event-stream' do
