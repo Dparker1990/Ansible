727ceb07debe3274306e5f9a1c3e75216603f7e1
Derek Parker <parkerderek86@gmail.com>
2013-06-06 21:22:35 -0500

    Modify on_load calls

--- lib/ansible.rb
+++ lib/ansible.rb
@@ -3,10 +3,10 @@ require 'ansible/constant_resolver'
 require 'ansible/transmit_model'
 require 'ansible/transmit_controller'
 
-ActiveSupport.on_load(:active_record) do
-  extend Ansible::TransmitModel
+ActiveSupport.on_load(:action_controller) do
+  include Ansible::TransmitController
 end
 
-ActiveSupport.on_load(:action_controller) do
-  extend Ansible::TransmitController
+ActiveSupport.on_load(:active_record) do
+  extend Ansible::TransmitModel
 end
